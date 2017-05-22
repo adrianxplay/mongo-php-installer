a14caca47699c5da84c0d3e96c9f9cea67724892
vagrant <vagrant@homestead>
2017-05-22 18:31:54 +0000

    script

---
+++ mongo-php-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Importing mongo public key"
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "Creating mongodb-enterprise.list file"
+echo "deb [ arch=amd64,arm64,ppc64el,s390x ] http://repo.mongodb.com/apt/ubuntu xenial/mongodb-enterprise/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
+echo "updating respositories"
+apt-get update
+echo "Installing mongodb"
+apt-get install -y mongodb-enterprise
+echo "Initializing mongo service"
+service mongod start
+echo "Installing pkg-config"
+apt-get install pkg-config
+echo "Installing mongodb php extension"
+pecl install mongodb
